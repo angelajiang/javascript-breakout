df70f879186addb12725b7416947e35b3ad63aa4
Angela Jiang <angelajiang2014@u.northwestern.edu>
2013-11-30 20:03:50 -0500

    Refactored globals, q table updates, added display modules. Need to automate starting games, egreedy learning

--- server.py
+++ server.py
@@ -11,101 +11,124 @@ from flask import Flask, request
 app = Flask(__name__)
 
 ### GLOBALS ###
-curState = dict(paddleX=0, ballX=0, ballV=0)
-lastState = dict()
+CURSTATE = dict(paddleX=1, ballX=1, ballY = 1, ballV=1, move=2)
+LASTSTATE = dict()
 #Tweakable parameters
 #table
-tableFile = 'table'
-dim = dict()
-dim['numActions'] = 3
-dim['maxPaddleX'] = 24 #0-24
-dim['maxBallX'] = 30   #0-30
-dim['maxBallY'] = 24   #0-24
-dim['maxBallV'] = 6
+TABLEFILE = 'table'
+NUMACTIONS = 3
+MAXPADDLEX = 24 #0-24
+MAXBALLX = 30   #0-30
+MAXBALLY = 24   #0-24
+MAXBALLV = 6
 #velocity mappings
-vMap = dict()
-vMap['downLeft'] = 1
-vMap['down'] = 2
-vMap['downRight'] = 3
-vMap['upLeft'] = 4
-vMap['up'] = 5
-vMap['upRight'] = 6
+DOWNLEFT = 1
+DOWN = 2
+DOWNRIGHT = 3
+UPLEFT = 4
+UP = 5
+UPRIGHT = 6
 
 #algorithm
-defaultReward = .1
-alpha = 1
-decay = .9
-greedyProbability = .5
+DEFAULTREWARD = .1
+ALPHA = 1
+DECAY = .9
+GREEDYPROB = .5
 
+#create_table must be before QTABLE references it
 @app.route('/create_table/<filename>')
 def create_table(filename):
     #Creates persistent q-table. Writes table object to filename
-    global numActions
-    global maxPaddleX
-    global maxBallX
-    global maxBallV
-    global defaultReward
+    global DEFAULTREWARD
+    global NUMACTIONS
+    global MAXPADDLEX
+    global MAXBALLX
+    global MAXBALLY
+    global MAXBALLV
 
     #+1 for zero indexing
-    table = np.ones(shape=(numActions, maxPaddleX+1, maxBallX+1, maxBallY+1, maxBallV)) * defaultReward
+    table = np.ones(shape=(NUMACTIONS, MAXPADDLEX+1, MAXBALLX+1, MAXBALLY+1, MAXBALLV)) * DEFAULTREWARD
     f = open(filename, 'w+')
     pickle.dump(table, f);
     f.close()
     return "Table stored in filename: " + filename
 
 #q-table
-qTable = None
-while (qTable == None):
+QTABLE = None
+while (QTABLE == None):
     try:
-        f = open(tableFile, 'r')
-        qTable = pickle.load(f)
+        f = open(TABLEFILE, 'r')
+        QTABLE = pickle.load(f)
         f.close()
     except:
-        create_table(tableFile)
+        create_table(TABLEFILE)
 
 ### MODULES ###
 def shrink(curVal, shrinkedMax, maxVal):
    return int((curVal/maxVal)*shrinkedMax)
 
 def indexTable(state, action):
+   global QTABLE
    paddleX = state['paddleX']
    ballX = state['ballX']
+   ballY = state['ballY']
    ballV = state['ballV']
    #-1 for zero indexing
-   return qTable[action, paddleX-1, ballX-1, ballV-1]
+   return QTABLE[action, paddleX-1, ballX-1, ballY-1, ballV-1]
+
+def updateTable(state, action, value):
+    global QTABLE
+    paddleX = state['paddleX']
+    ballX = state['ballX']
+    ballY = state['ballY']
+    ballV = state['ballV']
+    #-1 for zero indexing
+    QTABLE[action, paddleX-1, ballX-1, ballY-1, ballV-1] = value
+    return
 
 def eGreedy(state):
+    #Chooses a move due to eGreedy
     leftVal = indexTable(state, 0)
     rightVal = indexTable(state, 1)
     stayVal = indexTable(state, 2)
-    if (rightVal > leftVal):
+    maxVal = max(leftVal, rightVal, stayVal)
+    if (rightVal == maxVal):
         return 1
-    elif (rightVal == leftVal):
-        return 2
-    else:
+    elif (leftVal == maxVal):
         return 0
+    else:
+        return 2
 
 def maxQ(state):
     leftVal = indexTable(state, 0)
     rightVal = indexTable(state, 1)
     stayVal = indexTable(state, 2)
-    if (rightVal > leftVal):
-        return rightVal
-    elif (rightVal == leftVal):
-        return stayVal
-    else:
-        return leftVal
+    return max(leftVal, rightVal, stayVal)
 
 def signal_handler(signal, frame):
-    #qTable written to file after ctrl-c
+    #QTABLE written to file after ctrl-c
     frame = sys._getframe(0)
-    global qTable
-    global tableFile
-    f = open(tableFile, 'w')
-    pickle.dump(qTable, f)
+    global QTABLE
+    global TABLEFILE
+    f = open(TABLEFILE, 'w')
+    pickle.dump(QTABLE, f)
     f.close()
     sys.exit(0)
     return 
+
+def updateQ(state, stateMaxQ, reward):
+    #Calculates q value that Q(s,a) of last state should be
+    #Calls updateTable to update
+    global DEFAULTREWARD
+    global ALPHA
+    global DECAY
+    global GREEDYPROB
+
+    action = state['move']
+    lastQ = indexTable(state, action)
+    q = (1-ALPHA)*lastQ + ALPHA*(reward+DECAY*stateMaxQ)
+    updateTable(state, action, q)
+    return
 
 ### ROUTABLE MODULES ###
 
@@ -116,30 +139,38 @@ def send_file(filename):
 
 @app.route('/get_move', methods=['POST'])
 def get_move():
-    global qTable
-    global curState
-    global lastState
-    global dim
-    global vMap
+    global QTABLE
+    global CURSTATE
+    global LASTSTATE
+    global MAXBALLY
+    global UP
+    global UPLEFT
+    global UPRIGHT
 
-    lastState = curState.copy()
+    #Game over?
+    ballY = int(request.values["ballY"])
+    if (ballY >= MAXBALLY):
+        return "stay"
 
     #Get current state
-    curState['paddleX'] = int(request.values["paddleX"])
-    curState['ballX'] = int(request.values["ballX"])
-    curState['ballV'] = int(request.values["ballV"])
-    curState['ballY'] = int(request.values["ballY"])
-
-    ballY = curState['ballY']
-    ballV = curState['ballV']
+    LASTSTATE = CURSTATE.copy()
+    CURSTATE['paddleX'] = int(request.values["paddleX"])
+    CURSTATE['ballX'] = int(request.values["ballX"])
+    CURSTATE['ballV'] = int(request.values["ballV"])
+    CURSTATE['ballY'] = int(request.values["ballY"])
 
     #Update last state
-    #if ballY = paddleY, reward = 1, else 0
-    if (ballY == dim['maxBallY']) and (ballV == vMap['up'] or ballV == vMap['upLeft'] or ballV == vMap['upRight']):
-        stateMaxQ = maxQ(curState)
-        #updateQ(lastState, reward);
+    ballV = CURSTATE['ballV']
+    if (ballY == MAXBALLY) and (ballV == UP or ballV == UPLEFT or ballV == UPRIGHT):
+        reward = 1
+    else:
+        reward = 0
+    stateMaxQ = maxQ(CURSTATE)
+    updateQ(LASTSTATE, stateMaxQ, reward);
 
-    move = eGreedy(curState)
+    #Get move for current state
+    move = eGreedy(CURSTATE)
+    CURSTATE['move'] = move
     if (move == 1):
         return "right"
     elif (move == 2):
@@ -150,17 +181,49 @@ def get_move():
 @app.route('/update_table')
 def update_table():
     #For debugging to check if updates are written to file
-    global qTable
-    global tableFile
-    qTable = qTable * 2
-    tableStr = np.array_str(qTable)
+    global QTABLE
+    global TABLEFILE
+    QTABLE = QTABLE * 2
+    tableStr = np.array_str(QTABLE)
     return tableStr
 
-@app.route('/print_table')
-def print_table():
-    global qTable
-    tableStr = np.array_str(qTable)
-    return tableStr
+@app.route('/disp/<obj>')
+def disp(obj):
+    global QTABLE
+    global LASTSTATE
+    global CURSTATE
+    if (obj == 'curstate'):
+        state = CURSTATE
+        paddleX = state['paddleX']
+        ballX = state['ballX']
+        ballY = state['ballY']
+        ballV = state['ballV']
+        return str(paddleX) + ' ' + str(ballX) + ' ' + str(ballY) + ' ' + str(ballV)
+    elif (obj == 'laststate'):
+        state = LASTSTATE
+        paddleX = state['paddleX']
+        ballX = state['ballX']
+        ballY = state['ballY']
+        ballV = state['ballV']
+        return str(paddleX) + ' ' + str(ballX) + ' ' + str(ballY) + ' ' + str(ballV)
+    elif (obj == 'lastq'):
+        state = LASTSTATE
+        paddleX = state['paddleX']
+        ballX = state['ballX']
+        ballY = state['ballY']
+        ballV = state['ballV']
+        leftVal = indexTable(LASTSTATE, 0)
+        rightVal = indexTable(LASTSTATE, 1)
+        stayVal = indexTable(LASTSTATE, 2)
+        statestr = str(paddleX) + ' ' + str(ballX) + ' ' + str(ballY) + ' ' + str(ballV)
+        qstr = str(leftVal) + ' ' + str(rightVal) + ' ' + str(stayVal)
+        return "state: " + statestr + '\n' + "q vals: " + qstr
+    elif (obj == 'table'):
+        tableStr = np.array_str(QTABLE)
+        return tableStr
+    else:
+        return 'did not understand obj parameter'
+
 
 if __name__ == '__main__':
     signal.signal(signal.SIGINT, signal_handler)
